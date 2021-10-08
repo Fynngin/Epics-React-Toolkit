0348bcc6b6ef617e5d0e032777fbe72ed12feed4
Fynn Knoop <fknoop@techfak.de>
2021-10-08 09:28:48 +0200

    Added store pack display

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -2,22 +2,42 @@ import { useEffect, useState } from "react"
 import { getStorePacks } from "../api/api";
 import { useAuth } from "../App"
 import { Pack } from "../interfaces/Pack";
+import StorePackDisplay from "./StorePackDisplay";
+
+function filterActivePacks(packs: Pack[]) {
+    const today = new Date();
+    return packs.filter(pack => {
+        if (pack.purchaseEnd !== undefined) {
+            if (new Date(pack.purchaseEnd) > today && pack.inventoryCount > 0)
+                return true;
+        }
+        return false;
+    })
+}
+
+function sortPacksByInventoryCount(packs: Pack[]) {
+    return packs.sort((a, b) => b.inventoryCount - a.inventoryCount)
+}
 
 export default function Home() {
     const auth = useAuth();
     const [packs, setPacks] = useState([] as Pack[]);
 
     useEffect(() => {
-        const initStorePacks = async (jwt: string) => {
-            const storePacks = await getStorePacks(jwt, 1);
-            setPacks(storePacks);
+        const initStorePacks = async () => {
+            const storePacks = await getStorePacks(auth.jwt, 1);
+            const activePacks = filterActivePacks(storePacks);
+            const sortedPacks = sortPacksByInventoryCount(activePacks);
+            setPacks(sortedPacks);
         }
-        initStorePacks(auth.jwt);
+        initStorePacks();
     }, [auth]);
 
     return(
         <div>
-            <p>{packs.length}</p>
+            <div className='p-4'>
+                <StorePackDisplay packs={packs}/>
+            </div>
         </div>
     )
 }
---
+++ src/Home/StorePack.tsx
@@ -0,0 +1,28 @@
+import { Pack } from "../interfaces/Pack"
+
+function getPackImage(pack: Pack): string {
+    const primaryImage = pack.images.find(img => img.name === 'image');
+    if (primaryImage)
+        return primaryImage.url;
+    return pack.images[0].url;
+}
+
+export default function StorePack({ pack }: { pack: Pack }) {
+    return(
+        <div className='w-36 border-2 border-green-700 rounded-md flex flex-col font-mono'>
+            <div className='flex-grow text-center p-2 bg-green-700'>
+                <p>{pack.name}</p>
+            </div>
+            <div className='flex p-2'>
+                <p>Stock: {pack.inventoryCount}</p>
+            </div>
+            <div className='flex-shrink p-2'>
+                <img 
+                    className=''
+                    src={`https://cdn.epics.gg${getPackImage(pack)}`} 
+                    alt='No pack preview found :('
+                />
+            </div>
+        </div>
+    )
+}
