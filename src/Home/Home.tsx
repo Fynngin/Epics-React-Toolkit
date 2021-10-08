51a132b4df2169925ddf6d145ced430e7a1f3e50
Fynn Knoop <fknoop@techfak.de>
2021-10-08 16:04:51 +0200

    Removed transitions and added container for store packs

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -3,6 +3,7 @@ import { getStorePacks } from "../api/api";
 import { useAuth } from "../App"
 import { Pack } from "../interfaces/Pack";
 import StorePackDisplay from "./StorePackDisplay";
+import { CurrencyDollarIcon } from '@heroicons/react/solid';
 
 function filterActivePacks(packs: Pack[]) {
     const today = new Date();
@@ -35,7 +36,11 @@ export default function Home() {
 
     return(
         <div>
-            <div className='p-4'>
+            <div className='m-2 border-2 border-gray-500 rounded-md'>
+                <div className='pl-2 h-10 bg-gray-500 font-bold'>
+                    <CurrencyDollarIcon className='h-full inline'/>
+                    <h1 className='ml-2 inline-block align-middle'>STORE PACKS</h1>
+                </div>
                 <StorePackDisplay packs={packs}/>
             </div>
         </div>
--- src/Home/StorePackDisplay.tsx
+++ src/Home/StorePackDisplay.tsx
@@ -1,16 +1,13 @@
-import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { Pack } from "../interfaces/Pack";
 import StorePack from "./StorePack";
-import './Transitions.css';
+
 
 export default function StorePackDisplay({ packs }: {packs: Pack[]}) {
     return(
-        <TransitionGroup className='transition-enter transition-enter-active grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-12 grid-rows-5 gap-4 justify-items-center'>
+        <div className='p-2 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-12 grid-auto-rows gap-4 justify-items-center'>
             {packs.map(pack => (
-                <CSSTransition key={pack.id} addEndListener={(() => {})} classNames='transition'>
-                    <StorePack key={pack.id} pack={pack}/>
-                </CSSTransition>
+                <StorePack pack={pack}/>
             ))}
-        </TransitionGroup>
+        </div>
     )
 }
