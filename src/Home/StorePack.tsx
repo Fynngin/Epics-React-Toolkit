533c749eb8b41b4f4c289eac2cffa58459ab6182
Fynn Knoop <fknoop@techfak.de>
2021-10-14 16:19:10 +0200

    Added pack type icons to store packs

--- src/Home/StorePack.tsx
+++ src/Home/StorePack.tsx
@@ -1,3 +1,4 @@
+import { Avatar, Card, CardHeader, CardMedia, Typography } from "@mui/material";
 import { Pack } from "../interfaces/Pack"
 
 function getPackImage(pack: Pack): string {
@@ -9,20 +10,28 @@ function getPackImage(pack: Pack): string {
 
 export default function StorePack({ pack }: { pack: Pack }) {
     return(
-        <div className='w-36 h-full border-2 border-green-700 rounded-md flex flex-col'>
-            <div className='flex-grow text-center p-2 bg-green-700'>
-                <p>{pack.name}</p>
-            </div>
-            <div className='flex p-2'>
-                <p>Stock: {pack.inventoryCount}</p>
-            </div>
-            <div className='flex-shrink p-2'>
-                <img 
-                    className=''
-                    src={`https://cdn.epics.gg${getPackImage(pack)}`} 
-                    alt='No pack preview found :('
-                />
-            </div>
-        </div>
+        <Card variant='outlined' sx={{width: '10em', height: '100%'}}>
+            <CardHeader 
+                sx={{ height: '4em' }}
+                avatar={
+                    <Avatar variant='square' src={`packTypeIcons/${pack.acquireType[0]}.svg`}/>
+                }
+                title={
+                    <Typography variant="body1" sx={{opacity: 1, fontSize: 12 }}>
+                        {pack.name}
+                    </Typography>
+                }
+            />
+            <CardMedia
+                sx={{ p: 2 }}
+                component='img'
+                height='140'
+                image={`https://cdn.epics.gg${getPackImage(pack)}`}
+                alt='No pack preview found'
+            />
+            <Typography variant="body2" sx={{ p: 2, textAlign: 'center' }}>
+                Stock: {pack.inventoryCount}
+            </Typography>
+        </Card>
     )
 }
