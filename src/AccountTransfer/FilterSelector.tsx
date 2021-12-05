1f8cfbb2bcef7a4176a4c2f5e4aed7305a32667a
Fynn Knoop <fknoop@techfak.uni-bielefeld.de>
2021-12-05 13:30:29 +0100

    Added styling to Season Selector

--- src/AccountTransfer/FilterSelector.tsx
+++ src/AccountTransfer/FilterSelector.tsx
@@ -1,18 +1,19 @@
 import BaseContainer from "../BaseComponents/BaseContainer";
+import './FilterSelector.css';
 
 export default function FilterSelector() {
     const seasons = ["Founders", "2018", "2019", "2020", "2021"];
 
     return(
         <BaseContainer title='Filter Seasons / Collections' description='Change which Seasons you want to include in the transfer.'>
-            <form>
+            <div className='seasonsContainer'>
                 {seasons.map(season => (
-                    <label key={season}>{season}
-                        <input type="checkbox"/>
-                        <span></span>
-                    </label>
+                    <label key={season} className="checkbox">
+                        <input type="checkbox" name="checkbox" />
+                        {season}
+                  </label>
                 ))}
-            </form>
+            </div>
         </BaseContainer>
     )
 }
