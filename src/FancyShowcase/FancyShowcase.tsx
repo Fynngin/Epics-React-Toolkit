2a963170d7a83930d78b606107e7b56980b09e19
Fynn Knoop <fknoop@techfak.uni-bielefeld.de>
2021-12-26 10:57:04 +0100

    Fixed Showcase Sorting

--- src/FancyShowcase/FancyShowcase.tsx
+++ src/FancyShowcase/FancyShowcase.tsx
@@ -3,16 +3,16 @@ import { DndContext, DragEndEvent, KeyboardSensor, PointerSensor, useSensor, use
 import { getMainShowcase } from "../api/api";
 import { useAuth } from "../App"
 import BaseContainer from "../BaseComponents/BaseContainer";
-import { Card } from "../interfaces/Card";
 import { Showcase } from "../interfaces/Showcase";
 import './FancyShowcase.css';
 import SortableItem from "./SortableItem";
 import CardImage from "./CardImage";
 import { arrayMove, SortableContext, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
+import { restrictToHorizontalAxis } from '@dnd-kit/modifiers';
 
 export default function FancyShowcase() {
-    // const [showcase, setShowcase] = useState({} as Showcase);
-    const [cardIds, setCardIds] = useState([] as string[]);
+    const [showcase, setShowcase] = useState<Showcase>();
+    const [showcaseIndices, setShowcaseIndices] = useState<string[]>([]);
     const auth = useAuth();
 
     const sensors = useSensors(
@@ -23,42 +23,41 @@ export default function FancyShowcase() {
     );
 
     useEffect(() => {
+        const getShowcase = async () => {
+            let res: Showcase = await getMainShowcase(auth.user.jwt, auth.user.id);
+            setShowcaseIndices(res.cards.map((_, idx: number) => idx.toString()))
+            setShowcase(res);
+        }
         getShowcase();
-    });
-    
-    async function getShowcase() {
-        let res: Showcase = await getMainShowcase(auth.user.jwt, auth.user.id);
-        setCardIds(res.cards.map(el => el.id.toString()));
-    }
+    }, [auth]);
 
     function handleDragEnd(event: DragEndEvent) {
         const {active, over} = event;
-        console.log(active)
         
-        // if (active.id !== over.id) {
-        //     setShowcase((showcase: Showcase) => {
-        //         const oldIndex = showcase.cards.indexOf(active.id);
-        //         const newIndex = showcase.cards.indexOf(over.id);
+        if (over && over.id && active.id !== over.id) {
+            setShowcaseIndices((indices: string[]) => {
+                const oldIndex = indices.indexOf(active.id);
+                const newIndex = indices.indexOf(over.id);
                 
-        //         return {...showcase, cards: arrayMove(showcase.cards, oldIndex, newIndex)};
-        //     });
-        // }
+                return arrayMove(indices, oldIndex, newIndex);
+            });
+        }
     }
 
     return(
         <div className='showcasePage'>
             <BaseContainer title='Showcase'>
-                {showcase.cards ?
-                <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
-                    <div className='showcaseContainer'>
-                        <SortableContext items={showcase.cards?.map((_, idx) => idx.toString())}>
-                            {showcase.cards?.map((card: Card, idx: number) => (
-                                <SortableItem id={card.id.toString()}>
-                                    <CardImage card={card} />
+                {showcase ?
+                <DndContext onDragEnd={handleDragEnd} sensors={sensors} modifiers={[restrictToHorizontalAxis]}>
+                    <SortableContext items={showcaseIndices}>
+                        <div className='showcaseContainer'>
+                            {showcaseIndices?.map((idx: string) => (
+                                <SortableItem id={idx} key={idx}>
+                                    <CardImage card={showcase.cards[Number.parseInt(idx)]} />
                                 </SortableItem>
                             ))}
-                        </SortableContext>
-                    </div>
+                        </div>
+                    </SortableContext>
                 </DndContext>
                 : <></>}
             </BaseContainer>
